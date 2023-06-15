36cb5508304c9f6848ea5d3eb355cea33a75d87b
itsMuli <rosemwendwa163@gmail.com>
2023-06-15 12:59:08 +0300

    changes

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './offers.css'
+
+const Offers = () => {
+  return (
+    <div>
+      offers
+    </div>
+  )
+}
+
+export default Offers
---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './popular.css'
+
+const Popular = () => {
+  return (
+    <div>
+      Popular
+
+    </div>
+  )
+}
+
+export default Popular
