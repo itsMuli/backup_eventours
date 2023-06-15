d7d7b24193899e9f9dd3af9f44b8a21d1e1173da
itsMuli <rosemwendwa163@gmail.com>
2023-06-15 13:30:04 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./app.css"
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
-import Main from './Components/Main/Main';
-
+import About from './Components/About/About';
+import Blog from './Components/Blog/Blog';
+import Offers from './Components/Offers/Offers';
+import Popular from './Components/Popular/Popular';
 
 const App = () => {
   return (
    <>
    <Navbar />
    {/* <Home />
-   <Main />
+   <Popular />
+   <Offers />
+   <About />
+   <Blog />
    <Footer /> */}
    </>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import "./navbar.css"
+import {SiYourtraveldottv} from 'react-icons/si'
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <section className='navBarSection'>
+      <div className='header'>
+
+        <div className='logoDiv'>
+          <a href='#' className='logo'>
+            <h1 className='flex'><SiYourtraveldottv className="icon"/>
+            EvenTours
+            </h1>
+          </a>
+        </div>
+        <div className="navBar">
+          <ul className='navLists flex'>
+
+            <li className='navItem'>
+              <a href="#" className='navLink'>Home</a>
+            </li>
+
+            <li className='navItem'>
+              <a href="#" className='navLink'>About</a>
+            </li>
+
+            <li className='navItem'>
+              <a href="#" className='navLink'>Popular Destinations</a>
+            </li>
+
+            <li className='navItem'>
+              <a href="#" className='navLink'>Offers</a>
+            </li>
+
+            <li className='navItem'>
+              <a href="#" className='navLink'>Blog</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
